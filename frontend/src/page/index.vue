ab3f3d87b56eddca1eea5ae16cccc60dc95ce3c6
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2025-08-18 22:17:49 +0700

    mod

--- frontend/src/page/index.vue
+++ frontend/src/page/index.vue
@@ -4,6 +4,29 @@ import { VisXYContainer, VisLine, VisAxis, VisCrosshair, VisTooltip, VisScatter,
 import { ref, onMounted, shallowRef } from 'vue';
 import { CalendarDate, DateFormatter, getLocalTimeZone } from '@internationalized/date';
 
+type Data = { id: number; timestamp: string; temperature: number };
+
+const showGraph = ref<boolean>(false);
+const selectItems = ref<string[]>(['Mangan', 'Bobin', 'Bubuk']);
+const selectedItems = ref<string>('');
+const selectNo = ref<string[]>(['Block 1', 'Block, 2', 'Block 3', 'Block 4', 'Block 5', 'Block 6']);
+const selectedNo = ref<string>('');
+const pageIndication = ref<string | undefined>('');
+const time = ref<string>();
+const date = ref<string>();
+const count = 20;
+
+const df = new DateFormatter('en-US', {
+  dateStyle: 'medium',
+});
+const dateModel = shallowRef({
+  start: new CalendarDate(2023, 1, 20),
+  end: new CalendarDate(2023, 1, 30),
+});
+
+const x = (_d: Data, i: number): number => i;
+const y = [(d: Data): number => d.temperature];
+
 const item = ref<TabsItem[]>([
   {
     label: 'Dashboard',
@@ -19,235 +42,58 @@ const item = ref<TabsItem[]>([
   },
 ]);
 
-const df = new DateFormatter('en-US', {
-  dateStyle: 'medium',
-});
+const toolTipFormat = (d: Data) => `<p class="text-xs">${d.temperature} C</p>`;
 
-const dateModel = shallowRef({
-  start: new CalendarDate(2023, 1, 20),
-  end: new CalendarDate(2023, 1, 30),
-});
-
-const selectItems = ref<string[]>(['Mangan', 'Bobin', 'Bubuk']);
-const selectedItems = ref<string>('');
-
-const selectNo = ref<string[]>(['Block 1', 'Block, 2', 'Block 3', 'Block 4', 'Block 5', 'Block 6']);
-const selectedNo = ref<string>('');
-
-const pageIndication = ref<string | undefined>('');
-const time = ref<string>();
-const date = ref<string>();
-const checkSlot = (item?: string) => {
+function checkSlot(item?: string) {
   pageIndication.value = item;
-};
+}
 
-const data: { value: number; timestamp: string }[] = [
-  {
-    value: 500,
-    timestamp: '10.30',
-  },
-  {
-    value: 200,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 500,
-    timestamp: '10.30',
-  },
-  {
-    value: 200,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 500,
-    timestamp: '10.30',
-  },
-  {
-    value: 200,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 500,
-    timestamp: '10.30',
-  },
-  {
-    value: 200,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 430,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-  {
-    value: 34,
-    timestamp: '10.30',
-  },
-  {
-    value: 120,
-    timestamp: '10.30',
-  },
-];
+function generateTimestamp(at: string | Date): string[] {
+  const date = new Date(at);
+  const formatDate = date.toLocaleDateString('en-US', {
+    day: '2-digit',
+    month: 'long',
+  });
 
-const x = (_d: { value: number; timestamp: string }, i: number): number => i + 1;
-const y = [(d: { value: number; timestamp: string }): number => d.value];
+  const time = date.toLocaleTimeString('id-ID', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
 
-const generateTime = (): { timestamp: string[]; current: number[] } => {
+  return [`${time}`, `${formatDate}`];
+}
+
+function generateTemperatureData(count: number): Data[] {
+  const data = [];
+  const startTime = new Date('2025-08-18T08:00:00Z');
+
+  for (let i = 0; i < count; i++) {
+    const timestamp = new Date(startTime.getTime() + i * 1 * 60 * 1000);
+    const temperature = 25 + Math.random() * 10;
+
+    data.push({
+      id: i + 1,
+      timestamp: timestamp.toISOString(),
+      temperature: parseFloat(temperature.toFixed(1)),
+    });
+  }
+
+  return data;
+}
+
+const data = generateTemperatureData(500);
+
+function xTickFormat(i: number): string {
+  const d = getShortData(data);
+  return generateTimestamp(d[i].timestamp)[0];
+}
+
+function xTickFormatL(i: number): string {
+  return generateTimestamp(data[i].timestamp)[0];
+}
+
+function generateTime(): { timestamp: string[]; current: number[] } {
   const now = new Date();
   return {
     timestamp: [
@@ -264,17 +110,23 @@ const generateTime = (): { timestamp: string[]; current: number[] } => {
         timeZone: 'Asia/Jakarta',
       }),
     ],
-    current: [now.getFullYear(), now.getMonth() + 1, now.getDate()]
+    current: [now.getFullYear(), now.getMonth() + 1, now.getDate()],
   };
-};
+}
+
+function getShortData(data: Data[]): Data[] {
+  let shortedData = [];
+  for (let i = data.length - 1; i > data.length - 10; i--) {
+    shortedData.unshift(data[i]);
+  }
+  return shortedData;
+}
 
 setInterval(() => {
   const { timestamp } = generateTime();
   time.value = timestamp[0];
   date.value = timestamp[1];
 }, 60000);
-
-const count = 20;
 
 onMounted(() => {
   const { timestamp, current } = generateTime();
@@ -284,18 +136,22 @@ onMounted(() => {
   dateModel.value.start = new CalendarDate(current[0], current[1], current[2]);
   dateModel.value.end = new CalendarDate(current[0], current[1], current[2]);
 });
-
-const showGraph = ref<boolean>(false);
 </script>
 <template>
   <main class="relative flex h-screen">
-    <section class="flex flex-col justify-start gap-10 bg-white text-slate-700 w-fit p-10 pr-14 pnsc-light overflow-auto">
-      <img src="../assets/logo/Panasonic_logo_blue.png" alt="" width="120" class="mr-auto" />
-      <UTabs :items="item" orientation="vertical" class="w-full" variant="link" size="lg">
-        <template #content="{ item }">
-          <p>{{ checkSlot(item.label) }}</p>
-        </template>
-      </UTabs>
+    <section class="flex flex-col justify-between bg-white text-slate-700 pl-10 py-10 pnsc-light overflow-auto">
+      <div class="flex flex-col justify-start gap-10 w-fit">
+        <img src="../assets/logo/Panasonic_logo_blue.png" alt="" width="120" class="mr-auto" />
+        <UTabs :items="item" orientation="vertical" class="w-full" variant="link" size="lg">
+          <template #content="{ item }">
+            <p>{{ checkSlot(item.label) }}</p>
+          </template>
+        </UTabs>
+      </div>
+      <div class="flex flex-col items-start gap-1">
+        <p class="pnsc-light text-xs text-slate-500">Developed by</p>
+        <h1 class="pnsc-bold leading-4 text-blue-800">PRODUCTION ENGINEERING</h1>
+      </div>
     </section>
     <UCard class="w-full overflow-y-auto" variant="soft">
       <template #header>
@@ -316,12 +172,14 @@ const showGraph = ref<boolean>(false);
           </div>
         </div>
         <div class="flex gap-1.5 flex-wrap justify-start">
-          <UCard v-for="i in count" class="flex flex-col gap-5" :class="count < 3 ? 'w-full' : 'w-96'">
+          <UCard v-for="i in count" class="flex flex-col gap-5" :class="count < 3 ? 'w-full' : 'w-95'">
             <h1 class="text-xs pnsc-light mb-2">Oven {{ i }} {{ selectedItems }}</h1>
-            <VisXYContainer :data="data" :height="180">
+            <VisXYContainer :data="getShortData(data)" :height="180">
               <VisLine :x="x" :y="y" :lineWidth="2" />
-              <VisAxis type="x" :x="x" :gridLine="false" label="Time" labelFontSize="11px" tickTextFontSize="12px" />
+              <VisAxis type="x" :x="x" :gridLine="false" label="Time" labelFontSize="11px" tickTextFontSize="12px" :tickFormat="xTickFormat" :numTicks="5" :tickTextAngle="15"/>
               <VisAxis type="y" :y="x" label="Temp(C)" labelFontSize="11px" tickTextFontSize="12px" />
+              <VisCrosshair :template="toolTipFormat" />
+              <VisTooltip />
             </VisXYContainer>
           </UCard>
         </div>
@@ -362,11 +220,13 @@ const showGraph = ref<boolean>(false);
         </section>
         <section class="flex-2 h-[calc(100vh-120px)] flex flex-col gap-5">
           <UCard class="h-full flex justify-center items-center">
-            <UCard class="flex flex-col gap-5 w-[40rem]" variant="soft" v-if="showGraph">
-              <VisXYContainer :data="data" :height="500" :yDomain="[0, 600]">
+            <UCard class="flex flex-col gap-5 w-[45rem]" variant="soft" v-if="showGraph">
+              <VisXYContainer :data="data" :height="500">
                 <VisLine :x="x" :y="y" :lineWidth="2" />
-                <VisAxis type="x" :x="x" label="Time" labelFontSize="11px" tickTextFontSize="12px" :numTicks="20" />
-                <VisAxis type="y" :y="x" label="Temp(C)" labelFontSize="11px" tickTextFontSize="12px" :numTicks="10" />
+                <VisAxis type="x" :x="x" label="Time" labelFontSize="11px" tickTextFontSize="12px" :numTicks="10" :tickFormat="xTickFormatL" :tickTextAngle="30" />
+                <VisAxis type="y" :y="x" label="Temp(C)" labelFontSize="11px" tickTextFontSize="12px" />
+                <VisCrosshair :template="toolTipFormat" />
+                <VisTooltip />
               </VisXYContainer>
             </UCard>
             <h1 v-else class="text-slate-400 text-lg">Result will appear here</h1>
